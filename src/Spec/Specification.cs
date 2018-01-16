48097e63b4aca8be59c9db93c718d9335b39c10b
Mick Roper <mick.roper@peoplesholdings.co.uk>
2018-01-16 09:13:30 +0000

    added spec class

---
+++ src/Spec/Specification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spec
+{
+    /// <summary>
+    /// Base class for all specifications
+    /// </summary>
+    /// <typeparam name="T">The type being tested by this candidate</typeparam>
+    public abstract class Specification<T>
+    {
+        /// <summary>
+        /// Tests if this specification is satisfied by a candidate
+        /// </summary>
+        /// <param name="candidate">The candidate</param>
+        /// <returns></returns>
+        public abstract bool IsSatisfiedBy(T candidate);
+    }
+}
