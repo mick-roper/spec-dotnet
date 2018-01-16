53ed6dbbb8b35b9077989ffd299d1fb888fd92e6
Mick Roper <mick.roper@peoplesholdings.co.uk>
2018-01-16 09:15:29 +0000

    added composite spec

---
+++ src/Spec/CompositeSpecification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spec
+{
+    /// <summary>
+    /// A base class for specifications that compare 2 specs
+    /// </summary>
+    /// <typeparam name="TCandidate">The candidate type</typeparam>
+    public abstract class CompositeSpecification<TCandidate> : Specification<TCandidate>
+    {
+    }
+}
--- src/Spec/Specification.cs
+++ src/Spec/Specification.cs
@@ -7,14 +7,14 @@ namespace Spec
     /// <summary>
     /// Base class for all specifications
     /// </summary>
-    /// <typeparam name="T">The type being tested by this candidate</typeparam>
-    public abstract class Specification<T>
+    /// <typeparam name="TCandidate">The type being tested by this candidate</typeparam>
+    public abstract class Specification<TCandidate>
     {
         /// <summary>
         /// Tests if this specification is satisfied by a candidate
         /// </summary>
         /// <param name="candidate">The candidate</param>
         /// <returns></returns>
-        public abstract bool IsSatisfiedBy(T candidate);
+        public abstract bool IsSatisfiedBy(TCandidate candidate);
     }
 }
